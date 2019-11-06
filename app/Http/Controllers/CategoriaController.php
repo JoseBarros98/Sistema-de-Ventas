e15dad68c36e1dc0818f4207d9307c4c092c3f58
José Barros <barrosrojasjoserodolfo90@gmail.com>
2019-11-06 00:15:39 -0400

    Categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; 
+use App\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Request\CategoriaFormRequest;
+use DB;
+
+class CategoriaController extends Controller
+{
+    public function __construct(){
+
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if($request){
+            $query=trim($request->get('searchText'));
+            $categorias=DB::table('categoria')->where('nombre','LIKE','%'.$query.'%')
+            ->where('condicion','=','1')
+            ->orderBy('idcategoria','desc')
+            ->paginate(7);
+            return view('almacen.categira.index',["categorias"=>$categorias,"searchText"=>$query]);
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return viwe("almacen.categoria.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoriaFormRequest $request)
+    {
+        $categoria=new Categoria;
+        $categoria->nombre=$request->get('nombre');
+        $categoria->descripcion=$request->get('descripcion');
+        $categoria->condicion='1';
+        $categoria->save();
+        return Redirect::to('almacen/categoria');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view("almacen.categoria.show",["categoria"=>Categoria::findOrFail($id)]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view("almacen.categoria.edit",["categoria"=>Categoria::findOrFail($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoriaFormRequest $request, $id)
+    {
+        $categoria=Categoria::findOrFail($id);
+        $categoria->nombre=$request->get('nombre');
+        $categoria->descripcion=$request->get('descripcion');
+        $categoria->update();
+        return Redirect::to('almacen/categoria');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categoria=Categoria::findOrFail($id);
+        $categoria->condicion='0';
+        $categoria->update();
+        return Redirect::to('almacen/categoria');
+    }
+}
